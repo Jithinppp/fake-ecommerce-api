e31b6dc9b9cc62e43c04f95529d64896886f1e76
Jithinppp <jithinp968@gmail.com>
2022-12-25 16:33:41 +0530

    added a link in home route

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const SHOPPING_DATA = require("./SHOPPING_DATA");
 
 const PORT = process.env.PORT || 3300;
 
+app.get("/", (req, res) => {
+  res.send(
+    `<a href="https://fake-ecommerce-api.vercel.app/products">JSON products</a>`
+  );
+});
+
 app.get("/products", (req, res) => {
   res.json(SHOPPING_DATA);
 });
